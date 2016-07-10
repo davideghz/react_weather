9b7a3bddf332badc016cfece344fb8afaa4e9fc0
Davide Ghezzi <davideghz@gmail.com>
2016-07-10 17:34:10 -0400

    add text to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -21,8 +21,21 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About component</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built in React.</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="http://facebok.github.io/react" target="_blank">React</a> - This is the JavaScript
+            framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used OpenWeatherMap to
+            search for weather data by city name.
+        </li>
+      </ul>
     </div>
 
   );
