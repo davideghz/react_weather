874bbe3d6a090240cdeb18edb64dc6fae5059091
Davide Ghezzi <davideghz@gmail.com>
2016-07-10 16:05:21 -0400

    fix server.js to work on locale

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 3000;
 
 // OpenWeatherMap only work on http, so we redirect https to http
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  }else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
-  }
+  }else {
+    next();
+  }  
 });
 
 app.use(express.static('public'));
